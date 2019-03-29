1d494a3934f3d1a7938eef476ea4e72871dbe7f5
lcnssantos <luciano.ssants@gmail.com>
2019-03-29 15:49:55 -0300

    Update timecount.c

--- timecount.c
+++ timecount.c
@@ -1,17 +1,32 @@
 #include "timecount.h"
 #include "main.h"
 
+/*
+@purpose: Time Count Construction
+@parameters: Time -> a pointer to the time count class
+@return: void
+@version: V0.1
+*/
+
 void TimeCountConstruct(Time * Time)
 {
     Time->Update = TimeCount_Update;
     Time->miliseconds = 0;
 }
 
+/*
+@purpose: Update the Time Count 
+@paramaeters: Time -> a pointer to the time count class
+@return: void
+@version: v0.1
+*/
 void TimeCount_Update(Time * Time)
 {
-    if (INTCONbits.TMR0IF) {
+    if (INTCONbits.TMR0IF) 
+    {
         Time->miliseconds = Time->miliseconds + 1;
-        if (Timer.PreloadOn) {
+        if (Timer.PreloadOn) 
+        {
             TMR0 = Timer.Preload;
         }
         INTCONbits.T0IF = 0;
